9cb97f95f1712b0d383c79f5e199d7aeda665024
Anurag Raj <adityanv8@gmail.com>
2025-09-13 02:33:05 +0530

    postgress setup

---
+++ packages/postgresdb/prisma/migrations/20250912204827_user_schema_created/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "Phone" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_Phone_key" ON "public"."User"("Phone");
